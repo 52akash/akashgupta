cdad4fd3f0ff44f4efd0953d832ea9cd66b977cb
Akash Gutpa <gupta52akash@hotmail.com>
2017-07-10 11:02:37 -0400

    weather app data retreval

--- weatherapp/script/script.js
+++ weatherapp/script/script.js
@@ -1,44 +1,78 @@
-$(document).ready(function() {
-  $("#btn").on('click', function(event) {
-    event.preventDefault();
-    getLatLon();
-  });
-});
+var units = {
+  metric: {
+    name: "Metric",
+    code: "C",
+    value: "Celsius"
+  },
+  imperial: {
+    name: "Imperial",
+    code: "F",
+    value: "Fahrenheit"
+  }
+};
 
-function getLatLon() {
-  $.ajax({
-    url: "http://ip-api.com/json",
-    type: "GET",
-    dataType: "json",
-    cache: false
-  }).done(function(data) {
-    $.ajax({
-        url: "http://api.openweathermap.org/data/2.5/weather?q=" + data.city + "&appid=5648c0aea6747ada27ac9941dece97d9&units=metric",
-        type: 'GET',
-        dataType: 'json',
-        cache: false,
-      })
-      .done(function(data) {
-        data = {
-          current: data.main,
-          weather: data.weather[0],
-          name: data.name,
-          country: data.sys.country
-        };
-        console.log(data);
-      }).error(function(data) {
-        console.log(data);
-      });
-  });
+var currUnt = units.metric;
+var weather;
+
+function initialize() {
+  $(".unit").html(currUnt.code);
+  weather = new Weather();
 }
 
-var weather = function functionName(data) {
-  var cityName = data.name;
-  var country = data.country;
-  var temp = data.current.temp;
-  var tempMax = data.current.temp_max;
-  var tempMin = data.current.temp_min;
+function registerEvents() {
+  //unit conversion and update
+  $('.unit').on('click', function(evt) {
+    evt.preventDefault();
+    currUnt = (currUnt === units.metric) ? units.imperial : units.metric;
+    $(".unit").html(currUnt.code);
+  });
+}
+$(document).ready(function() {
+  registerEvents();
+  initialize();
+
+  // var weather = getLatLon();
+  // if (weather != null) {
+  //   weather.update();
+  // }
+});
+
+function Weather(data) {
+  this.cityName = null;
+  this.country = null;
+  this.temp = null;
+  this.tempMax = null;
+  this.tempMin = null;
 
 
+  this.load = function() {
+    $.ajax({
+      url: "http://ip-api.com/json",
+      type: "GET",
+      dataType: "json",
+      cache: false
+    }).done(function(data) {
+      $.ajax({
+          url: "http://api.openweathermap.org/data/2.5/weather?q=" + data.city + "&appid=5648c0aea6747ada27ac9941dece97d9&units=" + currUnt.name,
+          type: 'GET',
+          dataType: 'json',
+          cache: false,
+        })
+        .done(function(data) {
+          cityName = data.name;
+          country = data.sys.country;
+          temp = data.main.temp;
+          tempMax = data.main.temp_max;
+          tempMin = data.main.temp_min;
+          $(".city").html(data.name + ", " + data.sys.country + new Date(data.dt * 1000).toLocaleTimeString());
+          $(".weather").html(data.weather[0].main);
+          $(".temp").html(data.main.temp + "&deg");
+        }).error(function(data) {
+          return null;
+        });
+    });
+  }
+
+  this.load();
 
 }
